9692ecaa61897c1214398e23dc8e48bd3e60d96c
unknown <padayattilsolomon@gmail.com>
2023-11-28 13:42:28 -0500

    navigation component created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom'
+import Navigation from './components/navigation/navigation.component';
+import Home from './components/products/product-list.component.jsx'
+import Cart from './components/cart/cart.component';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index element ={<Home/>} />
+        <Route path='cart' element ={<Cart/>} />
+      </Route>
+    </Routes>
   );
 }
 
